34182c738c016a6a8a573fa68fefdd49f3b047e6
Keif <mashidin@hotmail.com>
2014-04-22 18:55:59 -0400

    Initial commit.

---
+++ guestbook.py
@@ -0,0 +1,14 @@
+import cgi
+from google.appengine.api import users
+import webapp2
+
+MAIN_PAGE_HTML = """\
+<html>
+  <body>
+    <form action="/sign" method="post">
+      <div><textarea name="content" rows="3" cols="60"></textarea></div>
+      <div><input type="submit" value="Sign Guestbook"></div>
+    </form>
+  </body>
+</html>
+"""
